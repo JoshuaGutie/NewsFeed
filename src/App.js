35206d21ce35949a6b37334bc752f375cbd144bf
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-06-01 20:19:49 -0500

    mapped

--- src/App.js
+++ src/App.js
@@ -8,13 +8,51 @@ class App extends Component {
     super(props);
 
     this.state = {
-      articles: []
+      articles: [],
+      pageNumber: 0
     };
+
   }
+
+
+  infiniteScroll = () => {
+    // End of the document reached?
+    if (window.innerHeight + document.documentElement.scrollTop
+    === document.documentElement.offsetHeight){
+     
+       let newPage = this.state.page;
+       newPage++;
+        this.setState({
+             page: newPage
+        });
+       this.fetchData(newPage);
+       }
+    }
+
+  componentDidMount = () => {
+    this.fetchData(this.state.pageNumber);
+    window.addEventListener('scroll', this.infiniteScroll);
+    }
+
+  fetchData = (pageNum) => {
+    let articles = 'http://hn.algolia.com/api/v1/search?query='+pageNum;
+    fetch(articles)
+       .then(res=>res.json())
+       .then(data => {
+          this.setState({
+              articles: [...this.state.articles,...data.hits]
+          })
+          console.log("data", data)
+       })
+      }
+
+       
+
   render(){
   return (
     <div className="App">
-      
+      {this.state.articles.map(article => <div style = {{marginBottom: '200px'}} key = {article.created_at}> {article.title} </div>)}
+
     </div>
   );
 }
