75ff843a3ba8333e5f29c334bcc770690419706e
crashdaddy <kennethscoggins@mail.com>
2020-06-03 09:36:01 -0500

    fine tuning search queries

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.state = {
       articles: [],
       pageNumber: 0,
-      newsUrl: '//hn.algolia.com/api/v1/search?tags=front_page',
+      newsUrl: 'homeUrl',
       query: ''
     };
 
@@ -24,35 +24,29 @@ class App extends Component {
   changeUrl = (event) => {
     let newsUrl='';
     if (event==="homeUrl") newsUrl = '//hn.algolia.com/api/v1/search?tags=front_page';
-    if (event==="newUrl") newsUrl = '//hn.algolia.com/api/v1/search_by_date';
-    if (event==="risingUrl") newsUrl= '//hn.algolia.com/api/v1/search_by_date?tags=story';
-    
+    if (event==="newUrl") newsUrl = '//hn.algolia.com/api/v1/search_by_date?tags=story';
+    if (event==="risingUrl") newsUrl= '//hn.algolia.com/api/v1/search_by_date?numericFilters=points>=10&tags=story';
+
     this.setState({
       articles: [],
       pageNumber: 0,
-      newsUrl: newsUrl
+      newsUrl: event
     })
     this.fetchData(newsUrl,0);
   }
 
   // this handles the search form submission
   handleSubmit = (event) => {
-    const now = new Date()  
-    const secondsSinceEpoch = Math.round(now.getTime() / 1000) - 1000000000;
-    // special url for search queries
-    let newsUrl =`//hn.algolia.com/api/v1/search_by_date?query=${this.state.query}&tags=story&numericFilters=created_at_i>${secondsSinceEpoch}`
-    console.log(newsUrl);
     // don't reload the page
     event.preventDefault();
 
     // clear out existing articles
     this.setState({articles:[],
-    newsUrl: newsUrl,
+    newsUrl: 'queryUrl',
     pageNumber:0
     });
     // load new articles
-    this.fetchData(newsUrl, 0);
-    this.setState({query: ''});
+    this.fetchData('queryUrl', 0);
   }
 
   // keeps track of what the user's typing in the search box
@@ -84,7 +78,14 @@ class App extends Component {
   // calls the data
   fetchData = (queryUrl, pageNumber) => {
     // query url is the url for whatever api endpoint we need
-    let articles = queryUrl + `&page=${this.state.pageNumber}`;
+    let articles = queryUrl;
+    const now = new Date()  
+    const secondsSinceEpoch = Math.round(now.getTime() / 1000) - 1000000000;
+    if (articles==="homeUrl") articles = `//hn.algolia.com/api/v1/search?tags=front_page&page=${pageNumber}`;
+    if (articles==="newUrl") articles = `//hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`;
+    if (articles==="risingUrl") articles= `//hn.algolia.com/api/v1/search_by_date?numericFilters=points>=10&tags=story&page=${pageNumber}`;
+    if (articles==="queryUrl") articles = `//hn.algolia.com/api/v1/search_by_date?query=${this.state.query}&tags=story&numericFilters=created_at_i>${secondsSinceEpoch}&page=${pageNumber}`
+    console.log("articlesUrl:" + articles);
     // switched to axios to avoid CORS errors
     axios.get(articles)
       .then(data => {
