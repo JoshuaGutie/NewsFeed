c98b1fe251c8dd1b4da5cfe6bbcc5723325c2235
crashdaddy <kennethscoggins@mail.com>
2020-06-05 15:49:58 -0500

    css style info panel

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -18,7 +18,7 @@ function HN() {
 function Card({person}) {
 
     return(
-        <div>
+        <div className="infoCard">
             <a href={person.link} target="blank" style={{color:'black',textDecoration:'none'}}>
             <img src={person.img} alt='' className="imgPic" />
             <div style={{fontSize:'large',fontWeight:'bold'}}>{person.name}</div>
@@ -31,8 +31,8 @@ function Card({person}) {
 function InfoPanel({showInfo}) {
 
     return(
-        <div className="windowPane">
-        <div><Logo/><div id="closeButton" style={{position: 'absolute',top:'5px',right:'5px'}}>
+        <div className="windowPane" style={{padding:'0px',paddingBottom:'20px'}}>
+        <div className="infoHeader"><Logo/><div id="closeButton" style={{position: 'absolute',top:'5px',right:'5px'}}>
                 <img src={closeButton} alt="" style={{width:'30px'}} onClick={showInfo} />
                 </div>
         </div>
